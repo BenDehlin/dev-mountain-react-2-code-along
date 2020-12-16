14455028c28128c2c636d8414899b4d488599148
BenDehlin <ben.dehlin@gmail.com>
2020-12-16 10:51:29 -0700

    code along basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
+import "./App.css"
+import Header from "./Components/Header"
+import ListDisplay from "./Components/ListDisplay"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: "Matt",
+      todoList: ["Code the things", "Eat", "Sleep"],
+    }
+  }
+
+  render() {
+    return (
+      <section>
+        <Header />
+        <ListDisplay />
+      </section>
+    )
+  }
 }
 
-export default App;
+export default App
